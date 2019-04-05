74f0d656921d5001c942dad25201a2aa3525fd15
Zaccari S <zacosx@gmail.com>
2019-04-05 17:46:04 -0400

    Started coding some of the settings logic. Will work on it more in the near future.

---
+++ How Are You?/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  How Are You?
+//
+//  Created by Zaccari Silverman on 4/5/19.
+//  Copyright © 2019 RCOS-HowAreYou. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var colorHueView: UIView!
+    @IBOutlet weak var colorViewSlider: UISlider!
+    
+    @IBOutlet weak var deleteAllDataButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func deleteAllDataButtonPressed(_ sender: Any) {
+        
+        
+    }
+    
+    
+}
